43060c10fcda31c825b203129d20a7d1861b9093
bipnrai054 <bipinrai054@gmail.com>
2021-05-20 13:59:17 +0545

    button, display done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import BtnComponent from './components/BtnComponent';
 import DisplayComponent from './components/DisplayComponent';
 
 function App() {
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
   return (
     <div className='main-section'>
       <div className='clock-holder'>
         <div className='stopwatch'>
-          <DisplayComponent />
+          <DisplayComponent time={time} />
           <BtnComponent />
         </div>
       </div>
